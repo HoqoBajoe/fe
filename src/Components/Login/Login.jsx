63722be607edbff7f632af9d94c56ac647634ad8
GhifariADR <ghifariadr6@gmail.com>
2022-04-08 02:52:26 +0700

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -7,19 +7,21 @@ function Login(){
     return(
         
         <div className="container mb-5 ">
-            <div className="m-auto px-5 py-5 mt-5" id="div1">
+            <div className="row mx-auto px-3 py-5 mt-5 w-50 bg-white shadow align-items-center border" style={{height: "550px", minWidth: "330px", borderRadius: "10px"}}>
                 <form action="">
-                    <h1 className="text-center mb-5" style={{fontWeight: "bold"}} >Hoqo Bajoe</h1>
+                    <h1 className="text-center mb-5" style={{fontWeight: "bolder"}} >Hoqo Bajoe</h1>
                     <div className="mb-3">
-                        <label for="Username" className="form-label">Username</label>
+                        <label for="Username" className="form-label" style={{fontWeight: "bold"}}>Username</label>
                         <input type="text" className="form-control" style={{borderRadius: "10px"}} id="Username" ></input>
                     </div>
                     <div className="mb-3">
-                        <label for="password" className="form-label" >Password</label>
+                        <label for="password" className="form-label" style={{fontWeight: "bold"}} >Password</label>
                         <input type="password" className="form-control" style={{borderRadius: "10px"}} id="password"></input>
                     </div>
-                    <div className="mx-4">
-                        <button type="submit" className="mx-5 p-1 text-white" id="divb" >Login</button>
+                    <div className="row">
+                        <div className="col text-center">
+                            <button className="btn btn-default text-white w-50 h-50 mt-5 " style={{backgroundColor: "#091426", borderRadius: "10px", fontWeight: "bold", fontSize: "large"}}>Login</button>
+                        </div>
                     </div>
                     <div className="mt-3 text-center">
                         <a className="btn text-black" style={{textDecoration: "none", fontWeight: "bold"}}>Create Account</a>
